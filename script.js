59b54e64445e2addd498da9fc9bddb00693c6107
Rayhona <rayhonamirzoyeva.ru>
2024-01-27 15:34:45 +0500

    hgjgu

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-  
+// DO WHILE
+// let i = 0;
+//do{
+    //console.log(i);
+   // i++
+//}while (i < 10)
+
+// For
+//break; continue
+for (let j = 0; j < 100; j++){
+    if(j > 50) break
+    if(j % 10 === 0){ console.log(j);}
+    else{ continue
+        //break
+       console.log('Mos emas');
+    }
+}
+
+
+let arr = [1,5,12,18,20]
+for (let a = 0; a < arr.length; a++){
+    
+}
