e29556167390eb1a324fb47c373dd086b2d70227
Rayhona <rayhonamirzoyeva.ru>
2024-01-30 14:31:33 +0500

    heu

---
+++ homework.js
@@ -0,0 +1,46 @@
+
+// Homework
+
+//  Object-1
+let pupil1 = [
+  {
+    fullName: "Mirzoyeva Rayhona G`olibjonovna",
+    job: "Programmer",
+    age: 15,
+    birthday:" 2008.11.09"
+  },
+  {
+    fullName: "Mirzoyeva Rayhona G'olibjonovna",
+    age: 15,
+    job: "Programmer",
+    birthday:"2008.11.09"
+  },
+  {
+    fullName: "Ulmasova Ruxshona Maxmudovna",
+    age: 16,
+    job: "Desighner",
+    birthday:"2007.06.26"
+  },
+  {
+    fullName: "Ulmasova Ruxshona 'Maxmudovna'",
+    age: 16,
+    job: "Desighner",
+    birthday:"2007.06.26"
+  },
+  {
+    fullName: "Sunnatova Jasmina",
+    age: 15,
+    job: "Programmer",
+    birthday:"2008.11.27"
+  }
+]
+
+for (let a = 0; a < pupil1.length; a++) {
+  console.log(pupil1[a]);
+}
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,17 +1,18 @@
+
 // DO WHILE
 // let i = 0;
-//do{
-    //console.log(i);
-   // i++
-//}while (i < 10)
+// do{
+//     console.log(i);
+//    i++
+// }while (i < 10)
 
 // For
-//break; continue
+// break;//  continue
 for (let j = 0; j < 100; j++){
     if(j > 50) break
     if(j % 10 === 0){ console.log(j);}
-    else{ continue
-        //break
+    else{  continue
+    //     break
        console.log('Mos emas');
     }
 }
@@ -39,3 +40,9 @@ let  group = ["Shaxruz","Farid","Bobur","Shavkat","Xusan","Sardor","Laziz","Toli
 for (let a = 0; a < group.length; a++){
     console.log(group[a]+ ". 23-gurux talabasi");
 }
+
+let c = 0;
+do {
+    console.log(c);
+    c++
+}while (c < 21);
